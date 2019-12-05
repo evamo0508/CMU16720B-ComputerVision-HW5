44e0212ba0e31c724ff316209345827637464aa3
evamo0508 <evamo.tw1@gmail.com>
2019-12-05 05:35:33 -0500

    version before recording video

--- img_effect.py
+++ img_effect.py
@@ -133,7 +133,7 @@ def warp(img):
     # show the original and scanned images
     #print("STEP 3: Apply perspective transform")
     #cv2.imshow("Original", imutils.resize(orig, height = 650))
-    #cv2.imshow("Scanned", imutils.resize(warped, height = 650))
+    #cv2.imshow("Scanned", warped)
     #cv2.waitKey(0)
     #cv2.destroyAllWindows()
 
